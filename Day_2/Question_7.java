e5504a47908f9ca83c22dadd5fe179fd124ee281
Anurag1590 <anuragmishra1590@gmail.com>
2026-05-29 07:07:12 +0530

    This is my day 2 assignment

---
+++ Day_2/Question_7.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Question_7 {
+    public static void main(String[] args)
+    {
+        int n ;
+        Scanner Sc = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        n = Sc.nextInt();
+        int n1=n;
+        int product =1;
+        int r =0;
+        while(n>0)
+        {
+            r = n%10;
+            product = product*r;
+            n =n/10;
+        }
+        System.out.println("The product of digits of "+n1+" is "+product);
+    }
+}
+
