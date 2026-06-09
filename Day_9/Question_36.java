eb39e8c482255a0c91e060cc999b8fe6abe9320b
Anurag1590 <anuragmishra1590@gmail.com>
2026-06-09 06:59:22 +0530

    This is my day 9 assignment

---
+++ Day_9/Question_36.java
@@ -0,0 +1,36 @@
+//Code to print hollow square pattern. 
+import java.util.*;
+public class Question_36 {
+    public static void main(String[] args) {
+        int n ;
+        System.out.print("Enter n : ");
+        Scanner Sc= new Scanner(System.in);
+        n = Sc.nextInt();
+        for(int i =1;i<=n;i++)
+        {
+            if(i==1||i==n)
+            {
+                 for(int j =1;j<=n;j++)
+                {
+                    System.out.print("*");
+                }
+                    System.out.println();
+            }
+            else
+            {
+             for(int k =1;k<=n;k++)
+             {
+                if(k==1||k==n)
+                {
+                    System.out.print("*"); 
+                }
+                else
+                {
+                   System.out.print(" ");
+                }
+             }
+             System.out.println();
+            }
+        }
+}
+}
