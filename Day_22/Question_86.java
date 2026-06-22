cdd1005643da943c73bb204e84f19855dc7e81bd
Anurag1590 <anuragmishra1590@gmail.com>
2026-06-22 14:47:51 +0530

    This is my day 22 assignment.

--- Day_22/Question_86.java
+++ Day_22/Question_86.java
@@ -4,19 +4,19 @@ public class Question_86 {
    public static void main(String[] args) 
     {
         String s ;
-        int count =0;
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter  the string : ");
         s = sc.nextLine();
         int l = s.length();
-        for(int i=0;i<l;i++)
-        {
-          char ch = s.charAt(i);
-          if(ch!=' ')
+        if(s.trim().isEmpty()) 
           {
-            count++;
+          System.out.println("Number of words is zero");
           }
-        }
-       System.out.println("Number of words is : "+count);
+          else
+          {
+          String[] words = s.trim().split("\\s+");
+          int len = words.length;
+          System.out.println("Number of words is: " + len);
+         }
     }
 }
